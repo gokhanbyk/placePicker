0fe567d0f12db58ecabde137b7e56fb0fa15f0da
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-04-20 16:02:46 +0300

    optimizing state updates

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+function ProgressBar({ timer }) {
+  const [remainingTime, setRemainingTime] = useState(timer);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <progress value={remainingTime} max={timer} />
+  );
+}
+
+export default ProgressBar;
